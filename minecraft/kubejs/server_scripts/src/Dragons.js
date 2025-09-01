8f7a1031028afbce5b2bfec51546e20183ce0dac
B-Furze <93354443+B-Furze@users.noreply.github.com>
2025-09-01 10:30:24 -0400

    dragon egg recipes

---
+++ minecraft/kubejs/server_scripts/src/Dragons.js
@@ -0,0 +1,454 @@
+ServerEvents.recipes((event) => {
+    //all the different dragon egg recipes utilizing natures aura
+
+
+    //fire dragons
+    //red
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_red",
+            },
+            {
+                item: "minecraft:blaze_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_red",
+            },
+            {
+                item: "iceandfire:fire_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_red",
+            },
+            {
+                item: "minecraft:lava_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_red",
+            },
+            {
+                item: "minecraft:magma_block",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_red",
+        },
+        time: 600,
+    });
+    //emerald
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_green",
+            },
+            {
+                item: "minecraft:blaze_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_green",
+            },
+            {
+                item: "iceandfire:fire_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_green",
+            },
+            {
+                item: "minecraft:lava_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_green",
+            },
+            {
+                item: "minecraft:magma_block",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_green",
+        },
+        time: 600,
+    });
+    //bronze
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_bronze",
+            },
+            {
+                item: "minecraft:blaze_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_bronze",
+            },
+            {
+                item: "iceandfire:fire_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_bronze",
+            },
+            {
+                item: "minecraft:lava_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_bronze",
+            },
+            {
+                item: "minecraft:magma_block",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_bronze",
+        },
+        time: 600,
+    });
+    //gray
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_gray",
+            },
+            {
+                item: "minecraft:blaze_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_gray",
+            },
+            {
+                item: "iceandfire:fire_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_gray",
+            },
+            {
+                item: "minecraft:lava_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_gray",
+            },
+            {
+                item: "minecraft:magma_block",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_gray",
+        },
+        time: 600,
+    });
+
+    //ice dragons
+    //blue
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_blue",
+            },
+            {
+                item: "minecraft:powder_snow_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_blue",
+            },
+            {
+                item: "iceandfire:frost_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_blue",
+            },
+            {
+                item: "minecraft:water_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_blue",
+            },
+            {
+                item: "minecraft:ice",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_blue",
+        },
+        time: 600,
+    });
+    //white
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_white",
+            },
+            {
+                item: "minecraft:powder_snow_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_white",
+            },
+            {
+                item: "iceandfire:frost_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_white",
+            },
+            {
+                item: "minecraft:water_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_white",
+            },
+            {
+                item: "minecraft:ice",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_white",
+        },
+        time: 600,
+    });
+    //sapphire
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_sapphire",
+            },
+            {
+                item: "minecraft:powder_snow_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_sapphire",
+            },
+            {
+                item: "iceandfire:frost_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_sapphire",
+            },
+            {
+                item: "minecraft:water_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_sapphire",
+            },
+            {
+                item: "minecraft:ice",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_sapphire",
+        },
+        time: 600,
+    });
+    //silver
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_silver",
+            },
+            {
+                item: "minecraft:powder_snow_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_silver",
+            },
+            {
+                item: "iceandfire:frost_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_silver",
+            },
+            {
+                item: "minecraft:water_bucket",
+            },
+            {
+                item: "iceandfire:dragonscale_silver",
+            },
+            {
+                item: "minecraft:ice",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_silver",
+        },
+        time: 600,
+    });
+
+    //lightning dragons
+    //electric blue
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_electric",
+            },
+            {
+                item: "minecraft:lightning_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_electric",
+            },
+            {
+                item: "iceandfire:lightning_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_electric",
+            },
+            {
+                item: "minecraft:amethyst_block",
+            },
+            {
+                item: "iceandfire:dragonscale_electric",
+            },
+            {
+                item: "minecraft:lapis_lazuli",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_electric",
+        },
+        time: 600,
+    });
+    //amethyst
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_amythest",
+            },
+            {
+                item: "minecraft:lightning_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_amythest",
+            },
+            {
+                item: "iceandfire:lightning_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_amythest",
+            },
+            {
+                item: "minecraft:amethyst_block",
+            },
+            {
+                item: "iceandfire:dragonscale_amythest",
+            },
+            {
+                item: "minecraft:lapis_lazuli",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_amythest",
+        },
+        time: 600,
+    });
+    //copper
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_copper",
+            },
+            {
+                item: "minecraft:lightning_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_copper",
+            },
+            {
+                item: "iceandfire:lightning_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_copper",
+            },
+            {
+                item: "minecraft:amethyst_block",
+            },
+            {
+                item: "iceandfire:dragonscale_copper",
+            },
+            {
+                item: "minecraft:lapis_lazuli",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_copper",
+        },
+        time: 600,
+    });
+    //black
+    event.custom({
+        type: "naturesaura:tree_ritual",
+        ingredients: [
+            {
+                item: "iceandfire:dragonscale_black",
+            },
+            {
+                item: "minecraft:lightning_rod",
+            },
+            {
+                item: "iceandfire:dragonscale_black",
+            },
+            {
+                item: "iceandfire:lightning_lily",
+            },
+            {
+                item: "iceandfire:dragonscale_black",
+            },
+            {
+                item: "minecraft:amethyst_block",
+            },
+            {
+                item: "iceandfire:dragonscale_black",
+            },
+            {
+                item: "minecraft:lapis_lazuli",
+            },
+        ],
+        sapling: {
+            item: "minecraft:dragon_egg",
+        },
+        output: {
+            item: "iceandfire:dragonegg_black",
+        },
+        time: 600,
+    });
+});
